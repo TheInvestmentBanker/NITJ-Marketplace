e7fa11565fd3b256ab9158777ffb5a072a72e5c1
TheInvestmentBanker <rahulguptasmartman@gmail.com>
2025-09-09 00:04:01 +0530

    Latest

--- backend/server.js
+++ backend/server.js
@@ -23,5 +23,5 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use('/api/products', productRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
