d87af6a0d598c0593bb142225347523b6d3d57ab
TheInvestmentBanker <rahulguptasmartman@gmail.com>
2025-09-11 19:50:42 +0530

    Lat

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,3 @@
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
