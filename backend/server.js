305086bbd776d830c0167910441441f638e4bf7f
TheInvestmentBanker <rahulguptasmartman@gmail.com>
2025-09-11 18:19:46 +0530

    Lat

--- backend/server.js
+++ backend/server.js
@@ -43,8 +43,8 @@ const path = require("path");
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "frontend/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
   });
 }
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
